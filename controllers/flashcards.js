22189962a12b0134f0da27ac74606f88c0732d07
Dinny Nocentini <dinny.nocentini@gmail.com>
2020-01-20 18:43:31 -0600

    create a function to create a new flashcard

--- controllers/flashcards.js
+++ controllers/flashcards.js
@@ -1,10 +1,16 @@
 const Flashcard = require('../models/flashcard');
 
 module.exports = {
-    index
+    index,
+    create
 };
 
 async function index(req, res) {
     const flashcards = await Flashcard.find({});
     res.status(200).json(flashcards);
+};
+
+async function create(req, res) {
+    const flashcard = await Flashcard.create(req.body);
+    res.status(201).json(flashcard);
 };
