bdf5a6a8d264c1e2369dda9e5a185d9590a49bc3
Dinny Nocentini <dinny.nocentini@gmail.com>
2020-01-21 22:19:58 -0600

    create form to create a new flashcard

--- src/pages/AddFlashcardPage/AddFlashcardPage.jsx
+++ src/pages/AddFlashcardPage/AddFlashcardPage.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AddFlashcardPage extends Component {
     state = {
@@ -19,12 +19,80 @@ class AddFlashcardPage extends Component {
         this.props.handleAddFlashcard(this.state.formData)
     }
 
-    render () {
+    handleChange = e => {
+        const formData = { ...this.state.formData, [e.target.name]: e.target.value };
+        this.setState({
+            formData,
+            invalidForm: !this.formRef.current.checkValidity()
+        });
+    }
+
+    render() {
         return (
             <>
-            Add Flashcard
+            <h1>Add Flashcard</h1>
+            <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label>Question</label>
+                    <input
+                        className="form-control"
+                        name="question"
+                        value={this.state.formData.question}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Correct Answer</label>
+                    <input
+                        className="form-control"
+                        name="canswer"
+                        value={this.state.formData.canswer}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Wrong Answer 1</label>
+                    <input
+                        className="form-control"
+                        name="wanswer1"
+                        value={this.state.formData.wanswer1}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Wrong Answer 2</label>
+                    <input
+                        className="form-control"
+                        name="wanswer2"
+                        value={this.state.formData.wanswer2}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Wrong Answer 3</label>
+                    <input
+                        className="form-control"
+                        name="wanswer3"
+                        value={this.state.formData.wanswer3}
+                        onChange={this.handleChange}
+                        required
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="btn"
+                    disabled={this.state.invalidForm}
+                >
+                    ADD FLASHCARD
+                </button>
+            </form>
             </>
         )
     }
-}
+};
+
 export default AddFlashcardPage;
