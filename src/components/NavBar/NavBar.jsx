20cd3905cc5c4e8bc7bed98c4ba41def4680e657
Dinny Nocentini <dinny.nocentini@gmail.com>
2020-01-21 21:36:29 -0600

    create ADM nav bar link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,24 +5,27 @@ import "./NavBar.css";
 const NavBar = props => {
     let nav = props.user ? (
         <div>
+            <Link to="/adm-flashcards" className="NavBar-Link" >
+                ADM
+            </Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
             <Link to="" className="NavBar-link" onClick={props.handlelogout}>
                 LOG OUT
             </Link>
         </div>
     ) : (
-            <div>
-                <Link to="/login" className="NavBar-link">
-                    LOG IN
-                </Link>
-                &nbsp;&nbsp;|&nbsp;&nbsp;
-                <Link to="/signup" className="NavBar-link">
-                    SIGN UP
-                </Link>
-            </div>
-        );
-    // let nav = <div>something </div>;
+        <div>
+            <Link to="/login" className="NavBar-link">
+                LOG IN
+            </Link>
+            &nbsp;&nbsp;|&nbsp;&nbsp;
+            <Link to="/signup" className="NavBar-link">
+                SIGN UP
+            </Link>
+        </div>
+    );
 
-    return <div className="NavBar">{nav}</div>;
+return <div className="NavBar">{nav}</div>;
 };
 
 export default NavBar;
