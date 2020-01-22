14de429cb80210cda81b8d0687051b07b1e4a0a1
Dinny Nocentini <dinny.nocentini@gmail.com>
2020-01-22 15:38:27 -0600

    create flashcards api deleteOne function

--- src/services/flashcards-api.js
+++ src/services/flashcards-api.js
@@ -12,3 +12,9 @@ export function create(flashcard) {
         body: JSON.stringify(flashcard)
     }).then(res => res.json());
 }
+
+export function deleteOne(id) {
+    return fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE'
+    }).then(res => res.json());
+}
